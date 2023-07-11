584c92170d0369949683950582b62ca23b49a2d2
gameskorob <97470697+gameskorob@users.noreply.github.com>
2023-07-11 11:11:15 +0300

    Add files via upload

---
+++ korobka-golovolomka/scripts/project/scriptsInEvents.js
@@ -0,0 +1,33 @@
+
+
+
+const scriptsInEvents = {
+
+	async E_game_Event1_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#1a2940'
+	},
+
+	async E_game_Event1_Act2(runtime, localVars)
+	{
+		window.addEventListener("keydown", function(e) {
+		      if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+		          e.preventDefault();
+		      }
+		  }, false);
+	},
+
+	async E_level_Event1_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#1a2940'
+	},
+
+	async E_menu_Event1_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#35838d'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
